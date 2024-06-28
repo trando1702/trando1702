24846bd3056701184502ab5a162e9d672a8551c2
trando1702 <trando1702@gmail.com>
2024-06-28 08:46:11 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-export default {
-	build: {
-		rollupOptions: {
-			input: "src/content.js",
-			output: {
-				format: "iife",
-				name: "content",
-				dir: "dist",
-				entryFileNames: "[name].js",
-			},
-		},
-	},
-};
+import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        port: 3000,
+    },
+    preview: {
+        port: 3000,
+    },
+    base: "/udlbook",
+});
